eb503b1b935f9b20ec18b5b243d0bdd8df1335c4
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-12-04 09:24:55 +1100

    Add vars and dtypes

---
+++ javascript/1_vars_dtypes.js
@@ -0,0 +1,26 @@
+// Three ways to declare variables 
+var var1 = 'var' // Not block scoped
+let var2 = 'var2' // Block scoped
+const var3 = 'var3' // Block scoped, can't be changed once defined
+
+
+
+
+// JS has dynamic typing: data types are automatically assigned to variables 
+// (we don't have to define the type when devlaring the variable)
+
+// Data Types - Primitive
+// Primitive means non-object data types
+let number = 1 // Always floating point with a decimal, even if we don't see them
+let string = 'string'
+let booleanTrue = true
+let booleanFalse = false
+let Undefined = undefined // automatically assigned to variables that don't have a value assigned
+let Null = null
+
+// Data Types - Objects (non-primitives)
+let object = {}
+let array = []
+let func = 'function'
+
+console.log('test' + 1)
