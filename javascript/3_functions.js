ab73f2a36ed9d692bcc185f8a3777edf7679c2a8
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-12-04 10:27:59 +1100

    Add functions

---
+++ javascript/3_functions.js
@@ -0,0 +1,19 @@
+// function declaration
+const calculateAge = function(birthYear) {
+    console.log(2019 - birthYear)
+}
+
+// arrow function 
+const sum = (a, b) => {
+    console.log(a + b)
+}
+
+// arrow function syntax 2 
+// parentheses not needed when there is only one argument
+const powerTwo = num => {
+    console.log(num ** 2)
+}
+
+// arrow function syntax 3
+// block not needed when it all fits to one line
+const multiply = (num1, num2) => console.log(num1 * num2)
